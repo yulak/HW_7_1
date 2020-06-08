c45f87fb025c5bd0cf7f46c53e4f1f422383c9c5
yulak <dyulakov@mail.ru>
2020-06-08 23:21:05 +0300

    yes

--- src/HW_7/Main.java
+++ src/HW_7/Main.java
@@ -9,18 +9,10 @@ public class Main {
         cat[2] = new Cat("Дуся", 10);
         Plate plate = new Plate(100);
 
-            /*for (Cat i : cat){
-                if (i.getSatiety() == 0){
-                    i.eat(plate);
-
-                    System.out.println(i.getName() + " покушал");
-                }
-            }*/
-
         cat[0].eat(plate);
         cat[1].eat(plate);
         cat[2].eat(plate);
-        //plate.infoFood();
+        plate.infoFood();
 
 
 
